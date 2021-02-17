15b3b26da7cd9c602b00a0b678a3e49793558733
pack3 <pack3rman@gmail.com>
2021-02-17 18:32:46 +0100

    KURS-0002 - Code Formatting and git ignore

--- exchange-rate-api/src/main/java/pl/kurs/java/controller/ExchangeController.java
+++ exchange-rate-api/src/main/java/pl/kurs/java/controller/ExchangeController.java
@@ -1,43 +1,40 @@
 package pl.kurs.java.controller;
 
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import pl.kurs.java.model.CurrentInfo;
 import pl.kurs.java.model.ExchangeRequest;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("")
 public class ExchangeController {
-//STWORZYC LISTY ROZWIJALNE DO strony z wymiana walut
-@GetMapping("")
-    public String enterMainPage(@ModelAttribute ExchangeRequest request, ModelMap model){
-    RestTemplate restTemplate = new RestTemplate();
-    ResponseEntity<CurrentInfo> response = restTemplate.getForEntity(
-            "https://api.exchangeratesapi.io/latest?base=PLN", CurrentInfo.class);
-    CurrentInfo currentInfo = response.getBody();
-    List<String> all = new ArrayList<String>(currentInfo.getRates().keySet());
-    model.addAttribute("currents", all);
-
-    return "exchangeView";
-    }
-    @GetMapping("/ex")
-    public String ex(@ModelAttribute ExchangeRequest request, ModelMap model){
-    model.addAttribute("request",request);
+    //STWORZYC LISTY ROZWIJALNE DO strony z wymiana walut
+    @GetMapping("")
+    public String enterMainPage(@ModelAttribute ExchangeRequest request, ModelMap model) {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<CurrentInfo> response = restTemplate.getForEntity(
-                "https://api.exchangeratesapi.io/latest?base="+request.getCurrentTo(), CurrentInfo.class);
+                "https://api.exchangeratesapi.io/latest?base=PLN", CurrentInfo.class);
+        CurrentInfo currentInfo = response.getBody();
+        List<String> all = new ArrayList<String>(currentInfo.getRates().keySet());
+        model.addAttribute("currents", all);
+
+        return "exchangeView";
+    }
+
+    @GetMapping("/ex")
+    public String ex(@ModelAttribute ExchangeRequest request, ModelMap model) {
+        model.addAttribute("request", request);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<CurrentInfo> response = restTemplate.getForEntity(
+                "https://api.exchangeratesapi.io/latest?base=" + request.getCurrentTo(), CurrentInfo.class);
         CurrentInfo currentInfo = response.getBody();
         System.out.println(request);
         System.out.println(currentInfo);

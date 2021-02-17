08764fadeb3efe01023745154186e69b11f18787
pack3 <pack3rman@gmail.com>
2021-02-17 18:43:42 +0100

    KURS-0004 - Adding JavaDoc

--- exchange-rate-api/src/main/java/pl/kurs/java/StartAppExchange.java
+++ exchange-rate-api/src/main/java/pl/kurs/java/StartAppExchange.java
@@ -3,8 +3,16 @@ package pl.kurs.java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application
+ */
 @SpringBootApplication
 public class StartAppExchange {
+    /**
+     * Main method
+     *
+     * @param args  program arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(StartAppExchange.class, args);
     }
